f2c62e7a389f61f0586a8618afe9ca22ab00889d
dustmr <dustinmroe@gmail.com>
2014-11-22 00:23:41 +0000

    landing pages layout fixes before learning to append

--- app/actions.rb
+++ app/actions.rb
@@ -21,21 +21,16 @@ end
 #   redirect '/homepage'  
 # end 
 
-get '/' do      ##Loads homepage - JI
+get '/' do      
+##Loads homepage - JI
   @inprogress = Project.where(completed: false).limit(3)
   erb :index
 end
 
-get '/inprogress' do
-  
-  # <% @inprogress.each do |comic| %>       ##should go in Views area
-  #  <% comic.tiles.each do |image| %>
-  #  <img src="/tile/<%= puts image.id %>.png />
-  #  <% end %>
-  # <% end %>
-end
 
-get '/inprogress/:id' do              ## + button template (will need a corresponding field in respective erb files that gives button value = <%= project.id %>)
+
+get '/inprogress/:id' do              
+## + button template (will need a corresponding field in respective erb files that gives button value = <%= project.id %>)
   @comic_inprogress = Project.find params[:id]
   puts @comic_inprogress.tiles.each do |tile|
     '/tile/'+tile.id
@@ -56,16 +51,63 @@ get '/project/:id' do
   erb :'/projects'
 end
 
-post '/project' do     ## creating a new comic through save button below start a story
+post '/project' do     
+## creating a new comic through save button below start a story
   @project = Project.new(title: params[:title])
   @tile = Tile.new
   @tile.image_data = params[:image_data]
 
   @project.tiles << @tile 
   @project.save
+  redirect '/'
 end
 
 post '/project/:id/tile' do
   @tile = Tile.new(title: params[:title])
   @tile.save
 end
+
+get '/projects' do
+  @projects = Project.order(:created_at)
+  erb :'projects'
+end
+
+get '/projects/new' do
+  erb :'projects/new'
+end
+
+
+get '/login' do
+  erb :'login'
+end
+
+post '/login' do
+  username = params[:username]
+  password = params[:password]
+  user = User.where(username: username, password: password).first
+  if user
+    session[:user] = user.username
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+post '/signup' do
+  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/signup'
+  end
+
+  
+end
+
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -10,3 +10,18 @@ $(document).ready(function() {
     });
     
 });
+
+$(document).ready(function() {
+  $('.alert').delay(2000).fadeIn(300);
+
+  $('.fa-times-circle').click(function(){
+   $(this).parent('.alert').fadeOut(300);
+  });
+
+  $('.switch').click(function(){
+     $(this).children('i').toggleClass('fa-pencil');
+     $('.login').animate({height: "toggle", opacity: "toggle"}, "slow");
+     $('.register').animate({height: "toggle", opacity: "toggle"}, "slow");
+  });
+  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+});
