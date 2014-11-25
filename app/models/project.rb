495a256f8665151d97d468dbb09e5f2fc875837a
dustmr <dustinmroe@gmail.com>
2014-11-25 00:51:21 +0000

    close to final with no rating or comment

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
 	has_many :tiles
-	has_many :ratings
+  has_many :ratings
+	has_many :comments
 	# has_many :users, through: :ratings
 	# has_one :rating
 	validates :length, length: { maximum: 9 }, presence: true, numericality: true
